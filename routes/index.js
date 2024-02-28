d2c6b11b21e9846b7c1c1ad73934b96f981bba9e
leekihyeok <789rlgur@naver.com>
2024-02-28 16:40:46 +0900

    modify_controller_Cmain2

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,12 @@ const router = express.Router();
 const controller = require("../controllers/Cmain");
 
 router.get("/", controller.open);
+router.get("/login", controller.login);
+router.get("/main", controller.main);
+router.get("/join", controller.join);
+router.get("/myPage", controller.myPage);
+router.get("/post", controller.post);
+router.get("/postEdit", controller.postEdit);
+router.get("/profileEdit", controller.profileEdit);
 
 module.exports = router;
