54f594b07dc70389b6632ff61de7b70bcf5c06d9
MoonMina <mingke48@gmail.com>
2024-03-01 15:16:13 +0900

    로그인 회원가입 등등

--- models/index.js
+++ models/index.js
@@ -1,21 +1,17 @@
-'use strict';
+"use strict";
 
-const Sequelize = require('sequelize');
-const config = require(__dirname + '/../config/config.json')["development"];
+const Sequelize = require("sequelize");
+const config = require(__dirname + "/../config/config.json")["development"];
 
-console.log('config >> '+config);
+console.log("config >> ", config);
 const db = {};
 
 // Sequelize를 이용하여 sequelize 인스턴스를 생성
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config,
-);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.User = require("./User")(sequelize, Sequelize);
 
 module.exports = db;
