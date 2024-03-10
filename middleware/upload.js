8e481dd081b8dbf96675f9c368cde6f5e7764424
MoonMina <mingke48@gmail.com>
2024-03-10 15:18:07 +0900

    커밋

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,13 +1,29 @@
 const multer = require("multer");
 const path = require("path");
-const upload = multer({
-    dest: "uploads/profile",
-});
+// const upload = multer({
+//     dest: "uploads/profile",
+// });
+// const uploads = multer({
+//     dest: "uploads/post",
+// });
 
 const uploadProfile = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
             cb(null, "uploads/profile"); // 저장될 파일 경로 설정
+        },
+        filename: function (req, file, cb) {
+            const ext = path.extname(file.originalname); // 확장자를 담는 코드
+
+            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+const uploadPostPhoto = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads/post"); // 저장될 파일 경로 설정
         },
         filename: function (req, file, cb) {
             const ext = path.extname(file.originalname); // 확장자를 담는 코드
