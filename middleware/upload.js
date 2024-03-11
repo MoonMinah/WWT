fb891982997bb950df7cbd2ae20040820b415f92
MoonMina <mingke48@gmail.com>
2024-03-11 14:13:05 +0900

    Committing changes before switching branches

--- middleware/upload.js
+++ middleware/upload.js
@@ -20,19 +20,6 @@ const uploadProfile = multer({
     }),
     limits: { fileSize: 5 * 1024 * 1024 },
 });
-const uploadPostPhoto = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, "uploads/post"); // 저장될 파일 경로 설정
-        },
-        filename: function (req, file, cb) {
-            const ext = path.extname(file.originalname); // 확장자를 담는 코드
-
-            cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-        },
-    }),
-    limits: { fileSize: 5 * 1024 * 1024 },
-});
 
 const uploadPostPhoto = multer({
     storage: multer.diskStorage({
